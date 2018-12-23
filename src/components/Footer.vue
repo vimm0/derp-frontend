ddc7d75b840ff7139bf9bdb42e56281409d65a23
vimm0 <vimmrana0@gmail.com>
2018-12-23 16:22:11 +0545

    initialized and setup layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>footer</p>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- START NAV -->
+    <nav class="navbar is-white">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item brand-text" href="../">
+                    Bulma Admin
+                </a>
+                <div class="navbar-burger burger" data-target="navMenu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div id="navMenu" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="admin.html">
+                        Home
+                    </a>
+                    <a class="navbar-item" href="admin.html">
+                        Orders
+                    </a>
+                    <a class="navbar-item" href="admin.html">
+                        Payments
+                    </a>
+                    <a class="navbar-item" href="admin.html">
+                        Exceptions
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+    <!-- END NAV -->
+</template>
